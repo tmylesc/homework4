b1ae3abdee226ecdc37ea21cd238524a79fcddad
Thomas Coradetti <tmcoradetti@gmail.com>
2020-06-13 09:41:46 -0400

    added questions array to js

---
+++ script.js
@@ -0,0 +1,28 @@
+//Array containing all the questions
+var questionList = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _____.",
+        choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+        answer: "Parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+        answer: "Quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal/bash", "For loops", "Console log"],
+        answer: "Console log"
+    },
+];
